69a61d7a33a177e38d4ee2bf2facc17d87e0e5d4
Ty <tycherms@gmail.com>
2024-07-24 21:17:37 -0400

    v1 release

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
-const { app, BrowserWindow, Tray, globalShortcut, screen, Notification, ipcMain } = require('electron');
+const { app, BrowserWindow, Tray, globalShortcut, screen, Notification, ipcMain, Menu } = require('electron');
 const path = require('path');
 const axios = require('axios');
+const fs = require('fs');
 let store;
 let tray;
 let playerWindow;
 let tankWindow;
+let tankSelectorWindow;
 let settingsWindow;
 let settingsCogWindow;
 let isVisible = false;
@@ -12,18 +14,33 @@ let isVisible = false;
 async function initializeStore() {
   const Store = await import('electron-store');
   store = new Store.default();
+  
+  // Initialize store with default values if not set
+  if (!store.get('windowPositions')) {
+    const { width, height } = screen.getPrimaryDisplay().workAreaSize;
+    store.set('windowPositions', {
+      player: { width: 400, height: 300, x: 0, y: 0 },
+      tank: { width: 400, height: 300, x: width - 400, y: 0 },
+      tankSelector: { width: width - 800, height: 200, x: 400, y: 0 },
+      settingsCog: { width: 40, height: 40, x: width - 60, y: height - 60 }
+    });
+  }
+  if (!store.get('isVisible')) {
+    store.set('isVisible', false);
+  }
+  if (!store.get('selectedTank')) {
+    store.set('selectedTank', 'Dva');
+  }
 }
 
 async function createWindows() {
   await initializeStore();
  
-  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
-  const playerConfig = { width: 400, height: 300, x: 0, y: 0 };
-  const tankConfig = { width: 200, height: 100, x: width - 200, y: 0 };
-  const cogConfig = store.get('settingsCog', { width: 40, height: 40, x: width - 50, y: height - 50 });
+  const windowPositions = store.get('windowPositions');
+  isVisible = store.get('isVisible');
 
   playerWindow = new BrowserWindow({
-    ...playerConfig,
+    ...windowPositions.player,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
@@ -34,7 +51,18 @@ async function createWindows() {
   });
 
   tankWindow = new BrowserWindow({
-    ...tankConfig,
+    ...windowPositions.tank,
+    frame: false,
+    transparent: true,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  tankSelectorWindow = new BrowserWindow({
+    ...windowPositions.tankSelector,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
@@ -45,7 +73,7 @@ async function createWindows() {
   });
 
   settingsCogWindow = new BrowserWindow({
-    ...cogConfig,
+    ...windowPositions.settingsCog,
     frame: false,
     transparent: true,
     alwaysOnTop: true,
@@ -57,24 +85,25 @@ async function createWindows() {
 
   playerWindow.loadFile('player.html');
   tankWindow.loadFile('tank.html');
+  tankSelectorWindow.loadFile('tankSelector.html');
   settingsCogWindow.loadFile('settingsCog.html');
 
   playerWindow.setAlwaysOnTop(true, 'screen-saver');
   tankWindow.setAlwaysOnTop(true, 'screen-saver');
+  tankSelectorWindow.setAlwaysOnTop(true, 'screen-saver');
   settingsCogWindow.setAlwaysOnTop(true, 'screen-saver');
 
   playerWindow.setVisibleOnAllWorkspaces(true);
   tankWindow.setVisibleOnAllWorkspaces(true);
+  tankSelectorWindow.setVisibleOnAllWorkspaces(true);
   settingsCogWindow.setVisibleOnAllWorkspaces(true);
 
-  playerWindow.hide();
-  tankWindow.hide();
-  settingsCogWindow.hide();
-
-  settingsCogWindow.on('moved', () => {
-    const bounds = settingsCogWindow.getBounds();
-    store.set('settingsCog', bounds);
-  });
+  if (!isVisible) {
+    playerWindow.hide();
+    tankWindow.hide();
+    tankSelectorWindow.hide();
+    settingsCogWindow.hide();
+  }
 
   createSettingsWindow();
 }
@@ -99,15 +128,42 @@ function createSettingsWindow() {
   settingsWindow.hide();
 }
 
+function saveWindowPositions() {
+  const positions = {};
+  
+  if (playerWindow && !playerWindow.isDestroyed()) {
+    positions.player = playerWindow.getBounds();
+  }
+  
+  if (tankWindow && !tankWindow.isDestroyed()) {
+    positions.tank = tankWindow.getBounds();
+  }
+  
+  if (tankSelectorWindow && !tankSelectorWindow.isDestroyed()) {
+    positions.tankSelector = tankSelectorWindow.getBounds();
+  }
+  
+  if (settingsCogWindow && !settingsCogWindow.isDestroyed()) {
+    positions.settingsCog = settingsCogWindow.getBounds();
+  }
+  
+  if (Object.keys(positions).length > 0) {
+    store.set('windowPositions', positions);
+  }
+}
+
 function toggleWindows() {
   isVisible = !isVisible;
+  store.set('isVisible', isVisible);
   if (isVisible) {
     playerWindow.show();
     tankWindow.show();
+    tankSelectorWindow.show();
     settingsCogWindow.show();
   } else {
     playerWindow.hide();
     tankWindow.hide();
+    tankSelectorWindow.hide();
     settingsCogWindow.hide();
     settingsWindow.hide();
   }
@@ -123,18 +179,29 @@ app.whenReady().then(async () => {
   const username = store.get('username');
   if (username) {
     fetchPlayerData(username);
+    settingsCogWindow.webContents.send('update-username', username);
   }
 
   tray = new Tray(path.join(__dirname, 'icon.png'));
   tray.setToolTip('TankWatch');
 
+  const contextMenu = Menu.buildFromTemplate([
+    { label: 'Toggle', click: toggleWindows },
+    { label: 'Save', click: saveWindowPositions },
+    { label: 'Quit', click: () => {
+      saveWindowPositions();
+      app.quit();
+    }}
+  ]);
+
+  tray.setContextMenu(contextMenu);
   tray.on('click', toggleWindows);
 
-  globalShortcut.register('CommandOrControl+Shift+T', toggleWindows);
+  globalShortcut.register('Alt+T', toggleWindows);
 
   app.on('activate', function () {
     if (BrowserWindow.getAllWindows().length === 0) createWindows();
-  });
+  }); 
 
   // Show notification
   new Notification({ 
@@ -143,22 +210,36 @@ app.whenReady().then(async () => {
     icon: path.join(__dirname, 'icon.png'),
     silent: true
   }).show();
+
+  // Fetch tank data and send to tankSelectorWindow
+  fetchTankData();
+
+  // Load and set the previously selected tank
+  const selectedTank = store.get('selectedTank');
+  tankSelectorWindow.webContents.send('update-selected-tank', selectedTank);
+  tankWindow.webContents.send('update-selected-tank', selectedTank);
 });
 
 app.on('window-all-closed', function () {
   if (process.platform !== 'darwin') app.quit();
 });
 
+app.on('will-quit', () => {
+  saveWindowPositions();
+});
+
 ipcMain.on('submit-username', async (event, username) => {
   store.set('username', username);
   await fetchPlayerData(username);
   tankWindow.webContents.send('update-username', username);
+  settingsCogWindow.webContents.send('update-username', username);
 });
 
 ipcMain.on('update-username', async (event, newUsername) => {
   store.set('username', newUsername);
   await fetchPlayerData(newUsername);
   tankWindow.webContents.send('update-username', newUsername);
+  settingsCogWindow.webContents.send('update-username', newUsername);
 });
 
 ipcMain.on('toggle-settings', () => {
@@ -181,3 +262,30 @@ async function fetchPlayerData(username) {
     playerWindow.webContents.send('player-data-error', 'Failed to fetch player data');
   }
 }
+
+async function fetchTankData() {
+  try {
+    const response = await axios.get('https://overfast-api.tekrop.fr/heroes?role=tank');
+    const tankMatchups = JSON.parse(fs.readFileSync('tank_matchups.json', 'utf8'));
+    tankWindow.webContents.send('tank-data', { heroes: response.data, matchups: tankMatchups });
+    tankSelectorWindow.webContents.send('tank-data', { heroes: response.data, matchups: tankMatchups });
+  } catch (error) {
+    console.error('Error fetching tank data:', error);
+    tankWindow.webContents.send('tank-data-error', 'Failed to fetch tank data');
+    tankSelectorWindow.webContents.send('tank-data-error', 'Failed to fetch tank data');
+  }
+}
+
+ipcMain.on('select-tank', (event, tankName) => {
+  store.set('selectedTank', tankName);
+  tankSelectorWindow.webContents.send('update-selected-tank', tankName);
+  tankWindow.webContents.send('update-selected-tank', tankName);
+});
+
+app.on('before-quit', () => {
+  saveWindowPositions();
+});
+
+app.on('will-quit', () => {
+  globalShortcut.unregisterAll();
+});
